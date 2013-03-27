3403c6c65568a27822648b421dc7c3f4aa75deaa
awongh <akira@awongh.com>
2013-03-26 21:25:04 -0600

    oops added these by mistake

--- code/rails/rails-test-1/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-RailsTest1::Application.config.secret_token = '63d82a94fafc3f0227a883b37ae349acdd7fe7ec82a562028c322bab56c6773f1b5cf51df1c25ea382b4b0cf633fbe9aaea9c992b0962dad16c0993720fd9861'
